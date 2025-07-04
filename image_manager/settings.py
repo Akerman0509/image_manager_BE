56b2e61a03b105fa07ecd58e38b2daf339664c17
Akerman0509 <strayslugs0509@gmail.com>
2025-07-04 14:38:02 +0700

    add allow_hosts

--- image_manager/settings.py
+++ image_manager/settings.py
@@ -28,8 +28,10 @@ SECRET_KEY = "django-insecure-)-y%l@4sorofp3rm97d7@jxb+jmjs4rvo1dkqw_5&2bs!0&r4)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
